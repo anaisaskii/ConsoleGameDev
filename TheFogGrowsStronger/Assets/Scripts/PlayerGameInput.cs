7d1f2c6d49b0bb844c48cebb2d652dd4f8cc7fa4
Smark101 <samuelmarklew@outlook.com>
2025-03-06 16:10:43 +0000

    Corrected movement

--- TheFogGrowsStronger/Assets/Scripts/PlayerGameInput.cs
+++ TheFogGrowsStronger/Assets/Scripts/PlayerGameInput.cs
@@ -19,18 +19,14 @@ public class PlayerGameInput : MonoBehaviour, IAgentMovementInput
     {
         m_input.actions["Player/Move"].performed += OnMove;
         m_input.actions["Player/Move"].canceled += OnMove;
-        m_input.actions["Player/Look"].performed += OnLook;
-        m_input.actions["Player/Look"].canceled += OnLook;
-        m_input.actions["Player/Sprint"].performed += OnInput;
+        m_input.actions["Player/ToggleSprint"].performed += OnInput;
     }
 
     private void OnDisable()
     {
         m_input.actions["Player/Move"].performed -= OnMove;
         m_input.actions["Player/Move"].canceled -= OnMove;
-        m_input.actions["Player/Look"].performed -= OnLook;
-        m_input.actions["Player/Look"].canceled -= OnLook;
-        m_input.actions["Player/Sprint"].performed -= OnInput;
+        m_input.actions["Player/ToggleSprint"].performed -= OnInput;
     }
 
     private void OnLook(InputAction.CallbackContext context)
